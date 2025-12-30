11d14f99731ee0dae9f1bdf6c0b8192e8c148725
IvaBatur <161125041+IvaBatur@users.noreply.github.com>
2025-12-30 11:57:28 +0100

    Add files via upload

---
+++ Wa_vjezba5/pizza-express/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+import { connectToDatabase } from './db.js';
+import pizzeRouter from './routes/pizze.js';
+import narudzbeRouter from './routes/narudzbe.js';
+
+config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: 'http://localhost:5173'
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    poruka: 'Pizza app - MongoDB verzija',
+    endpointi: {
+      pizze: '/pizze (GET, POST)',
+      narudzbe: '/narudzbe (POST)'
+    }
+  });
+});
+
+
+app.use('/pizze', pizzeRouter);
+app.use('/narudzbe', narudzbeRouter);
+
+
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Pizza poslužitelj sluša na http://localhost:${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error(' Greška pri pokretanju servera:', error);
+    process.exit(1);
+  });
